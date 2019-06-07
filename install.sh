b13eee8db35af33c66b6dca61356282b11305ff9
Jorengarenar <>
2019-06-07 14:30:41 +0200

    'Deactivation' was dangerous

--- install.sh
+++ install.sh
@@ -4,21 +4,15 @@ declare dotfiles_dir="$(dirname $(realpath $0))"
 declare force_flag=$1
 
 linking() {
-    if [[ $3 == "X" ]]; then
-        if [[ -e $2 ]]; then
-            rm $2
-        fi
-    else
-        if [[ $force_flag == "-f" ]]; then
-            mkdir -p $HOME/dotfiles.old/
-            mv "$2" "$HOME/dotfiles.old/"
-            echo "Moved file $2 to directory $HOME/dotfiles.old/"
-        fi
+    if [[ $force_flag == "-f" ]]; then
+        mkdir -p $HOME/dotfiles.old/
+        mv "$2" "$HOME/dotfiles.old/"
+        echo "Moved file $2 to directory $HOME/dotfiles.old/"
+    fi
 
-        if [ ! -e $2 ]; then
-            mkdir -p "$(dirname $2)"
-            ln -sf $dotfiles_dir/$1 $2
-        fi
+    if [ ! -e $2 ]; then
+        mkdir -p "$(dirname $2)"
+        ln -sf $dotfiles_dir/$1 $2
     fi
 }
 
@@ -34,7 +28,6 @@ linking  other/feh_keys         $HOME/.config/feh/keys
 linking  other/gitconfig        $HOME/.gitconfig
 linking  other/gpg-agent.conf   $HOME/.gnupg/gpg-agent.conf
 linking  other/inputrc          $HOME/.inputrc
-linking  other/myclirc          $HOME/.myclirc                      "X"
 linking  other/profile          $HOME/.profile
 linking  other/tmux.conf        $HOME/.tmux.conf
 linking  other/Xresources       $HOME/.Xresources
@@ -43,3 +36,5 @@ linking  tig/tigrc              $HOME/.tigrc
 linking  vim/indent             $HOME/.vim/indent
 linking  vim/UltiSnips          $HOME/.vim/UltiSnips
 linking  vim/vimrc              $HOME/.vimrc
+
+# linking  other/myclirc          $HOME/.myclirc
