3910f8d5bcfac7fd41d880c1211f03aca443ecdd
Jorengarenar <>
2019-06-07 13:18:52 +0200

    Deactivation flag in install script

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
+
 declare dotfiles_dir="$(dirname $(realpath $0))"
 declare force_flag=$1
 
 linking() {
-    if [[ $force_flag == "-f" ]]; then
-        mkdir -p $HOME/dotfiles.old/
-        mv "$2" "$HOME/dotfiles.old/"
-        echo "Moved file $2 to directory $HOME/dotfiles.old/"
-    fi
+    if [[ $3 == "X" ]]; then
+        if [[ -e $2 ]]; then
+            rm $2
+        fi
+    else
+        if [[ $force_flag == "-f" ]]; then
+            mkdir -p $HOME/dotfiles.old/
+            mv "$2" "$HOME/dotfiles.old/"
+            echo "Moved file $2 to directory $HOME/dotfiles.old/"
+        fi
 
-    if [ ! -e $2 ]; then
-        mkdir -p "$(dirname $2)"
-        ln -sf $dotfiles_dir/$1 $2
+        if [ ! -e $2 ]; then
+            mkdir -p "$(dirname $2)"
+            ln -sf $dotfiles_dir/$1 $2
+        fi
     fi
 }
 
@@ -26,6 +34,7 @@ linking  other/feh_keys         $HOME/.config/feh/keys
 linking  other/gitconfig        $HOME/.gitconfig
 linking  other/gpg-agent.conf   $HOME/.gnupg/gpg-agent.conf
 linking  other/inputrc          $HOME/.inputrc
+linking  other/myclirc          $HOME/.myclirc                      "X"
 linking  other/profile          $HOME/.profile
 linking  other/tmux.conf        $HOME/.tmux.conf
 linking  other/vim.tigrc        $HOME/.tigrc
@@ -34,5 +43,3 @@ linking  other/zathurarc        $HOME/.config/zathura/zathurarc
 linking  vim/indent             $HOME/.vim/indent
 linking  vim/UltiSnips          $HOME/.vim/UltiSnips
 linking  vim/vimrc              $HOME/.vimrc
-
-# linking  other/myclirc          $HOME/.myclirc
