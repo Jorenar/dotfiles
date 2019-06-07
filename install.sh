6e75d15a47349c2439f867750de9c15a245f9390
Jorengarenar <>
2019-06-08 01:38:03 +0200

    Added deactivation option to install script
    Also some minor changes

--- install.sh
+++ install.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+# ------------------------------------------------
+
+# 1st argument - file in dotfiles dir
+# 2nd argument - location of link
+# 3rd argument - deactivation flag
+
+# ------------------------------------------------
+
+
 declare dotfiles_dir="$(dirname $(realpath $0))"
 declare force_flag=$1
 
-linking() {
-    if [[ $force_flag == "-f" ]]; then
-        mkdir -p $HOME/dotfiles.old/
-        mv "$2" "$HOME/dotfiles.old/"
-        echo "Moved file $2 to directory $HOME/dotfiles.old/"
-    fi
+# ------------------------------------------------
 
-    if [ ! -e $2 ]; then
-        mkdir -p "$(dirname $2)"
-        ln -sf $dotfiles_dir/$1 $2
+linking() {
+    if [[ $3 == "X" ]]; then
+        if [[ -L $2 && "$(readlink -f $2)" == "$dotfiles_dir/$1" ]]; then
+            rm $2
+        fi
+    else
+        if [[ $force_flag == "-f" ]]; then
+            mkdir -p $HOME/dotfiles.old/
+            mv "$2" "$HOME/dotfiles.old/"
+            echo "Moved file $2 to directory $HOME/dotfiles.old/"
+        fi
+
+        if [ ! -e $2 ]; then
+            mkdir -p "$(dirname $2)"
+            ln -sf $dotfiles_dir/$1 $2
+        fi
     fi
 }
 
+# ------------------------------------------------
 
 linking  gtk/gtk3_settings.ini  $HOME/.config/gtk-3.0/settings.ini
 linking  gtk/gtkrc-2.0          $HOME/.gtkrc-2.0
@@ -28,6 +46,7 @@ linking  other/feh_keys         $HOME/.config/feh/keys
 linking  other/gitconfig        $HOME/.gitconfig
 linking  other/gpg-agent.conf   $HOME/.gnupg/gpg-agent.conf
 linking  other/inputrc          $HOME/.inputrc
+linking  other/myclirc          $HOME/.myclirc                      "X"
 linking  other/profile          $HOME/.profile
 linking  other/tmux.conf        $HOME/.tmux.conf
 linking  other/Xresources       $HOME/.Xresources
@@ -36,5 +55,3 @@ linking  tig/tigrc              $HOME/.tigrc
 linking  vim/indent             $HOME/.vim/indent
 linking  vim/UltiSnips          $HOME/.vim/UltiSnips
 linking  vim/vimrc              $HOME/.vimrc
-
-# linking  other/myclirc          $HOME/.myclirc
