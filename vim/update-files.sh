6fbe192decae2d5f3894367ec918bd5477b83ce2
Jorengarenar <jorengarenar@protonmail.com>
2020-02-07 10:25:35 +0100

    Script for updating additional Vim files

---
+++ vim/update-files.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+dl() {
+    mkdir -p "$1"
+    (cd "$1" && curl -O "$2")
+}
+
+dl $VIMDOTDIR/syntax https://raw.githubusercontent.com/leafgarland/typescript-vim/master/syntax/typescript.vim
