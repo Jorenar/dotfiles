06ec5750b7d466b0ae1235a0970535c3b0a95791
Jorengarenar <jorengarenar@protonmail.com>
2020-03-01 01:46:49 +0100

    Minor corrections

--- _XDG/patch/install.sh
+++ _XDG/patch/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-# Some things may not work properly if this script is run
-# outside of $XDG_DOTFILES_DIR/install.sh !
+# `linking` function won't work if this file isn't sourced from main install script
 
 # Check if user has sudo privileges {{{1
 prompt_sudo=$(sudo -nv 2>&1)
@@ -16,7 +15,7 @@ installing=false
 if [ ! -f /etc/profile.d/profile_xdg.sh ]; then
     if [ $is_sudo = true ]; then
         read -p 'Do you wish to install root patches for XDG support for 'profile' file? [y/N] ' -n 1 -r; echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
+        if [ $REPLY = "y" -o $REPLY = "Y" ]; then
             installing=true
         fi
     fi
@@ -25,7 +24,7 @@ else
 fi
 
 if [ $installing == "true" ]; then
-    sudo ln -sf $dotfiles_dir/_XDG/patch/profile_xdg.sh /etc/profile.d/profile_xdg.sh
+    sudo ln -sf $DIR/_XDG/patch/profile_xdg.sh /etc/profile.d/profile_xdg.sh
 elif [ $installing != done ]; then
     linking  profile  $HOME/.profile
 fi
@@ -37,7 +36,7 @@ if which bash &> /dev/null; then
     if ! grep -Fxq "$A" /etc/bash.bashrc; then
         if [ $is_sudo = true ]; then
             read -p 'Do you wish to install root patches for XDG support for Bash? [y/N] ' -n 1 -r; echo
-            if [[ $REPLY =~ ^[Yy]$ ]]; then
+            if [ $REPLY = "y" -o $REPLY = "Y" ]; then
                 installing=true
             fi
         fi
@@ -49,7 +48,7 @@ fi
 if [ $installing = true ]; then
     sudo sh -c "printf '\n# Source bashrc from XDG location\n$A' >> /etc/bash.bashrc"
 elif [ $installing != done ]; then
-    linking  bash/bashrc  $HOME/.bashrc
+    linking  bashrc  $HOME/.bashrc
 fi
 
 # {{{1
