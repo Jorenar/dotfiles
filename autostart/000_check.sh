1b2738a5b7dd3d26f6a003966aeeb1b63443f7fb
Jorengarenar <jorengarenar@protonmail.com>
2020-03-09 06:58:25 +0100

    $TMPDIR

--- autostart/000_check.sh
+++ autostart/000_check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-if [ -f /tmp/executed_autostart ]; then
+if [ -f "${TMPDIR:-/tmp}/executed_autostart" ]; then
     export EXECUTED_AUTOSTART=true
 else
-    touch /tmp/executed_autostart
+    touch "${TMPDIR:-/tmp}/executed_autostart"
 fi
--- autostart/100_startx.sh
+++ autostart/100_startx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # If there is display and variable AUTO_STARTX is set then on tty1 `startx`
-if [ -n "$AUTO_STARTX" ] && [ -z $DISPLAY ] && [ $(tty) = /dev/tty1 ] && [ ! -f /tmp/disable_auto_startx ]; then
+if [ -n "$AUTO_STARTX" ] && [ -z $DISPLAY ] && [ $(tty) = /dev/tty1 ] && [ ! -f "${TMPDIR:-/tmp}/disable_auto_startx" ]; then
 
     # Optimus manager
     if sudo -n /usr/bin/prime-switch > /dev/null 2>&1; then
@@ -11,6 +11,6 @@ if [ -n "$AUTO_STARTX" ] && [ -z $DISPLAY ] && [ $(tty) = /dev/tty1 ] && [ ! -f 
 
     exec startx $XINITRC
 
-elif [ -f /tmp/disable_auto_startx ]; then
-    rm /tmp/disable_auto_startx
+elif [ -f "${TMPDIR:-/tmp}/disable_auto_startx" ]; then
+    rm "${TMPDIR:-/tmp}/disable_auto_startx"
 fi
