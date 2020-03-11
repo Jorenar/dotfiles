c574ae90dad16c304d0db2d9705a90a1d2b3b619
Jorengarenar <jorengarenar@protonmail.com>
2020-03-11 11:28:07 +0100

    Git branch in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,24 @@
 function fish_prompt
-    printf '(fish)%s%s@%s%s:%s%s%s>%s ' (set_color --bold) $USER (prompt_hostname) (set_color --dim) (prompt_pwd) (set_color normal) (set_color --bold) (set_color normal)
+    # +(shell)user@host:pwd:git_branch>
+
+    if set -q PROMPT_SHOW_SHELL
+        set show_shell "(fish)"
+    else
+        set show_shell ""
+    end
+
+    printf '%s%s%s%s%s@%s%s%s:%s%s%s%s>%s ' \
+            (set_color --dim) \
+            $show_shell \
+            (set_color normal) \
+            (set_color --bold) \
+            $USER \
+            (prompt_hostname) \
+            (set_color normal) \
+            (set_color --dim) \
+            (prompt_pwd) \
+            (set_color normal) \
+            (git rev-parse --abbrev-ref HEAD 2> /dev/null | sed -r 's/(.*)/:\1/') \
+            (set_color --bold) \
+            (set_color normal)
 end
