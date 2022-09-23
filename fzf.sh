5da8437cd96d71b642a7a15a12295d40bc30eac0
Jorengarenar <dev@joren.ga>
2022-09-23 09:59:50 +0200

    Legacy FZF key bindings path

--- fzf.sh
+++ fzf.sh
@@ -7,8 +7,12 @@ s="" # shell
 [ -n "$ZSH_NAME" ] && s="zsh"
 
 # Use fzf keybindings
-if [ -n "$s" ] && [ -f "/usr/share/fzf/key-bindings.$s" ]; then
-    source "/usr/share/fzf/key-bindings.$s"
+if [ -n "$s" ]; then
+    if [ -f "/usr/share/fzf/key-bindings.$s" ]; then
+        source "/usr/share/fzf/key-bindings.$s"
+    elif [ -f "/usr/share/doc/fzf/examples/key-bindings.$s" ]; then
+        source "/usr/share/doc/fzf/examples/key-bindings.$s"
+    fi
 fi
 
 export FZF_CTRL_T_OPTS="--preview '(cat {} || tree {}) 2> /dev/null | head -200'"
