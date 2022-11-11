3b69cac8894f565bca4025437c68698ad5a6189e
Jorengarenar <dev@joren.ga>
2022-11-11 14:34:05 +0100

    Improve XDG_RUNTIME_DIR fallback

--- env/autostart/20_basics.sh
+++ env/autostart/20_basics.sh
@@ -2,9 +2,11 @@
 
 if ! grep -Fxqs "executed_autostart" "$TMPFLAGS"; then
 
+    mkdir -p "$XDG_HISTORY_DIR" # create history dir
+
     if sudo -n rfkill > /dev/null 2>&1; then
-        [ -z "AUTO_BT" ] && sudo rfkill block bluetooth
-        [ -n "NO_WIFI" ] && sudo rfkill block wifi
+        [ -z "$AUTO_BT" ] && sudo rfkill block bluetooth
+        [ -n "$NO_WIFI" ] && sudo rfkill block wifi
     fi
 
     [ -x "$(command -v fcitx)" ] && fcitx -d
