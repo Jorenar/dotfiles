43476487f17ea830986b99b15030038c2bbf9a34
Jorengarenar <dev@joren.ga>
2023-05-18 00:12:58 +0200

    Remove unnecesary clout from env/autostart

--- env/autostart/20_basics.sh
+++ env/autostart/20_basics.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env sh
 
-if ! grep -Fxqs "executed_autostart" "$TMPFLAGS"; then
-
-    mkdir -p "$HISTORY_DIR" # create history dir
-
-    if sudo -n rfkill > /dev/null 2>&1; then
-        [ -z "$AUTO_BT" ] && sudo rfkill block bluetooth
-        [ -n "$NO_WIFI" ] && sudo rfkill block wifi
-    fi
-
-    [ -x "$(command -v fcitx)" ] && fcitx -d
-
-    [ -n "$DISPLAY" ] && [ -x "command -v xsetroot" ] && xsetroot -solid "#000000" # set background
-
-    echo "executed_autostart" >> "$TMPFLAGS"
-fi
+mkdir -p "$HISTORY_DIR"
--- env/autostart/99_startx.sh
+++ env/autostart/99_startx.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env sh
 
-# Optimus manager
-if grep -Fxqs "switched_gpu" "$TMPFLAGS" && sudo -n prime-switch > /dev/null 2>&1; then
-    sudo prime-switch
-    sed -i '/switched_gpu/d' "$TMPFLAGS"
-fi
-
 # If there's no display and variable NOAUTOSTARTX is empty then on tty1 start X
-if      [ -z "$NOAUTOSTARTX" ] && [ -z "$WSL_DISTRO_NAME" ] && \
-        [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]          && \
-        ! grep -Fxqs "autostartedx" "$TMPFLAGS"           ; then
-
-    echo "autostartedx" >> "$TMPFLAGS"
-    sed -i '/GPU /d' "$TMPFLAGS"
-    exec startX
-
-elif grep -Fxqs "autostartx=0" "$TMPFLAGS"; then
-    sed -i '/autostartx=0/d' "$TMPFLAGS"
+if [ -z "$NOAUTOSTARTX" ] && [ -z "$WSL_DISTRO_NAME" ] && \
+        [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
+then
+    for x in "${TMPDIR:-/tmp}"/.X11-*; do
+        [ ! -d "$x" ] && exec startX
+        break
+    done
 fi
