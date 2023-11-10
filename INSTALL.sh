eeff1abfbcf6e13ba19f432484e35acdaab638cb
Jorengarenar <jorengarenar@outlook.com>
2023-11-11 00:32:23 +0100

    Move config files into subdirectory

--- INSTALL.sh
+++ INSTALL.sh
@@ -24,12 +24,12 @@ abspath () (
 
 cd "$(dirname "$(abspath "$0")")" || exit 1
 
-. env/variables
-. env/user-dirs.dirs
+. config/env/variables
+. config/env/user-dirs.dirs
 
 
 install () (
-    src="$(abspath "$1")"
+    src="$1"
     op="$2"
     dest="$3"
     mode="$4"
@@ -43,15 +43,15 @@ install () (
 
 
     if [ "$gf_force" -eq 1 ] && [ -e "$dest" ]; then
-        if mkdir -p "$HOME/dotfiles.old/"; then
-            mv "$dest" "$HOME/dotfiles.old/" && \
-                echo "Moved file '$dest' to directory '$HOME/dotfiles.old/'"
+        old="$HOME/dotfiles.old/$src"
+        if mkdir -p "$old"; then
+            mv "$dest" "$old" && echo "Moved file '$dest' to directory '$old'"
         fi
     fi
 
     if [ ! -e "$dest" ]; then
         mkdir -p "$(dirname "$dest")"
-        sh -c "$action '$src' '$dest'"
+        sh -c "$action '$(abspath "$src")' '$dest'"
 
         [ -n "$mode" ] && chmod "$mode" "$dest"
     fi
@@ -69,75 +69,77 @@ install_bulk () (
 
 
 
-install  env/profile  %  "$HOME"/.profile
-install  templates/   @  "$XDG_TEMPLATES_DIR"
+install  config/env/profile  %  "$HOME"/.profile
+install  templates/          @  "$XDG_TEMPLATES_DIR"
 
 install_bulk "$XDG_CONFIG_HOME" << EOL
 
-    aerc/               @  aerc
-    asoundrc            @  alsa/asoundrc
-    bash/               @  bash
-    ccache.config       @  ccache/config
-    chktexrc            @  .chktexrc
-    commafeed.yml       @  commafeed.yml
-    ctags/              @  ctags
-    deno.json           @  deno.json
-    dosbox.conf         @  dosbox/dosbox.conf
-    env                 @  env
-    env/mimeapps.list   @  mimeapps.list
-    env/user-dirs.dirs  @  user-dirs.dirs
-    feh/                @  feh
-    fish/               @  fish
-    fonts.conf          @  fontconfig/fonts.conf
-    gdbinit             @  gdb/gdbinit
-    git/                @  git
-    grip.py             @  grip/grip.py
-    gtkrc-2.0           @  gtk-2.0/gtkrc
-    htoprc              @  htop/htoprc                          -w
-    i3/                 @  i3
-    inputrc             @  readline/inputrc
-    lesskey             @  lesskey
-    mailcap             @  mailcap
-    mpv/                @  mpv
-    muttrc              @  mutt/muttrc
-    myclirc             @  mycli/myclirc
-    newsboat            @  newsboat/config
-    npmrc               @  npm/npmrc
-    OpenSCAD.conf       @  OpenSCAD/OpenSCAD.conf               -w
-    pavucontrol.ini     %  pavucontrol.ini
-    polybar             @  polybar/config
-    python_config.py    @  python/config.py
-    QuiteRss.ini        %  QuiteRss/QuiteRss.ini
-    ranger.conf         @  ranger/rc.conf
-    Renviron            @  R/Renviron
-    sh/                 @  sh
-    shellcheckrc        @  shellcheckrc
-    spicy_settings      @  spicy/settings
-    sqliterc            @  sqlite3/sqliterc
-    ssh_config          @  ssh/config
-    ssr.conf            %  simplescreenrecorder/settings.conf
-    stalonetrayrc       @  stalonetrayrc
-    telnetrc            @  .telnetrc
-    tigrc               @  tig/config
-    tmux/               @  tmux
-    transmission.json   %  transmission-daemon/settings.json
-    uncrustify/         @  uncrustify
-    vim/                @  vim
-    weechat/            @  weechat                              -w
-    X11/                @  X11
-    zathurarc           @  zathura/zathurarc
-    zsh                 @  zsh
+    config/aerc/               @  aerc
+    config/asoundrc            @  alsa/asoundrc
+    config/bash/               @  bash
+    config/ccache.config       @  ccache/config
+    config/chktexrc            @  .chktexrc
+    config/commafeed.yml       @  commafeed.yml
+    config/ctags/              @  ctags
+    config/deno.json           @  deno.json
+    config/dosbox.conf         @  dosbox/dosbox.conf
+    config/env                 @  env
+    config/env/mimeapps.list   @  mimeapps.list
+    config/env/user-dirs.dirs  @  user-dirs.dirs
+    config/feh/                @  feh
+    config/fish/               @  fish
+    config/fonts.conf          @  fontconfig/fonts.conf
+    config/gdbinit             @  gdb/gdbinit
+    config/git/                @  git
+    config/grip.py             @  grip/grip.py
+    config/gtkrc-2.0           @  gtk-2.0/gtkrc
+    config/htoprc              @  htop/htoprc                          -w
+    config/i3/                 @  i3
+    config/inputrc             @  readline/inputrc
+    config/lesskey             @  lesskey
+    config/mailcap             @  mailcap
+    config/mpv/                @  mpv
+    config/muttrc              @  mutt/muttrc
+    config/myclirc             @  mycli/myclirc
+    config/newsboat            @  newsboat/config
+    config/npmrc               @  npm/npmrc
+    config/OpenSCAD.conf       @  OpenSCAD/OpenSCAD.conf               -w
+    config/pavucontrol.ini     %  pavucontrol.ini
+    config/polybar             @  polybar/config
+    config/python_config.py    @  python/config.py
+    config/QuiteRss.ini        %  QuiteRss/QuiteRss.ini
+    config/ranger.conf         @  ranger/rc.conf
+    config/Renviron            @  R/Renviron
+    config/sh/                 @  sh
+    config/shellcheckrc        @  shellcheckrc
+    config/spicy_settings      @  spicy/settings
+    config/sqliterc            @  sqlite3/sqliterc
+    config/ssh_config          @  ssh/config
+    config/ssr.conf            %  simplescreenrecorder/settings.conf
+    config/stalonetrayrc       @  stalonetrayrc
+    config/telnetrc            @  .telnetrc
+    config/tigrc               @  tig/config
+    config/tmux/               @  tmux
+    config/transmission.json   %  transmission-daemon/settings.json
+    config/uncrustify/         @  uncrustify
+    config/vim/                @  vim
+    config/weechat/            @  weechat                              -w
+    config/X11/                @  X11
+    config/zathurarc           @  zathura/zathurarc
+    config/zsh                 @  zsh
 
 EOL
 
 install_bulk "$XDG_DATA_HOME" << EOL
 
-    app.desktop.d/           @  applications/custom
-    firefox/user.js          @  firefox/user.js
-    firefox/userChrome.css   @  firefox/chrome/userChrome.css
-    firefox/userContent.css  @  firefox/chrome/userContent.css
-    fonts/                   @  fonts
-    themes/                  @  themes
+    config/firefox/user.js          @  firefox/user.js
+    config/firefox/userChrome.css   @  firefox/chrome/userChrome.css
+    config/firefox/userContent.css  @  firefox/chrome/userContent.css
+
+    misc/desktop_entries/  @  applications/custom
+
+    fonts/          @  fonts
+    themes/         @  themes
 
 EOL
 
