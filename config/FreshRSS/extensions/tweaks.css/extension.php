543e6ebd816557485645331da69365d304ed9793
Jorengarenar <jorengarenar@outlook.com>
2023-12-02 02:17:34 +0100

    Remove FreshRSS config

--- config/FreshRSS/extensions/tweaks.css/extension.php
+++
@@ -1,7 +0,0 @@
-<?php
-class TweaksCSSExtension extends Minz_Extension {
-	public function init() {
-        Minz_View::appendStyle($this->getFileUrl('tweaks.css', 'css'));
-	}
-}
-?>
--- config/FreshRSS/extensions/tweaks.js/extension.php
+++
@@ -1,7 +0,0 @@
-<?php
-class TweaksJSExtension extends Minz_Extension {
-	public function init() {
-        Minz_View::appendScript($this->getFileUrl('tweaks.js', 'js'));
-	}
-}
-?>
