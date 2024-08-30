0edd13be540216375202364d77dc9321af1bf5b4
Jorengarenar <jorengarenar@outlook.com>
2024-08-30 22:24:47 +0200

    Launching gpg-agent from .profile file

---
+++ config/profile.d/50-gpg-agent.sh
@@ -0,0 +1,14 @@
+if [ ! -x "$(command -v gpg-agent)" ]; then
+    return
+fi
+
+gpg_id="${PASSWORD_STORE_DIR:-"$HOME"/.password-store}"/.gpg-id
+[ ! -s "$gpg_id" ] && return
+key="$(cat "$gpg_id")"
+
+tmp="${TMPDIR:-/tmp}/gpg.dummy.$$"
+
+echo | gpg --encrypt --recipient "$key" > "$tmp" 2> /dev/null
+gpg --decrypt "$tmp" > /dev/null 2>&1
+
+rm "$tmp"
