7f3b17ffa842f634fa95e71754d208034d0d344b
Jorenar <dev@jorenar.com>
2025-06-28 22:54:42 +0200

    Tweaks

--- bin/init-gpg-agent.sh
+++ bin/init-gpg-agent.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env sh
 
-init_gpg_agent () (
-    if [ ! -x "$(command -v gpg-agent)" ]; then
-        return
-    fi
+if [ ! -x "$(command -v gpg-agent)" ]; then
+    exit 1
+fi
 
-    gpg_id="${PASSWORD_STORE_DIR:-"$HOME"/.password-store}"/.gpg-id
-    [ ! -s "$gpg_id" ] && return
-    key="$(cat "$gpg_id")"
+gpg_id="${PASSWORD_STORE_DIR:-"$HOME"/.password-store}"/.gpg-id
+[ ! -s "$gpg_id" ] && exit 1
+key="$(cat "$gpg_id")"
 
-    tmp="${TMPDIR:-/tmp}/gpg.dummy.$$"
+tmp="${TMPDIR:-/tmp}/gpg.dummy.$$"
 
-    sleep 1; echo | gpg --encrypt --recipient "$key" > "$tmp" 2> /dev/null
-    sleep 1; gpg --decrypt "$tmp" > /dev/null 2>&1
+sleep 1; echo | gpg --encrypt --recipient "$key" > "$tmp" 2> /dev/null
+sleep 1; gpg --decrypt "$tmp" > /dev/null 2>&1
 
-    rm "$tmp"
-)
-
-init_gpg_agent
+rm "$tmp"
