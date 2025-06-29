4d0e781c89fe015ebf42d62a8e64c089738c50e8
Jorenar <dev@jorenar.com>
2025-06-29 23:05:44 +0200

    Move list of Mason packages to Neovim config

---
+++ config/nvim/init.d/mason-list.lua
@@ -0,0 +1,47 @@
+local packages = {
+  -- LSP {{{1
+  "arduino-language-server",
+  "asm-lsp",
+  "autotools-language-server",
+  "bash-language-server",
+  "deno",
+  "gopls",
+  "harper-ls",
+  "jedi-language-server",
+  "lua-language-server",
+  "m68k-lsp-server",
+  "openscad-lsp",
+  "sonarlint-language-server",
+  "sqls",
+  "texlab",
+  "vim-language-server",
+
+  -- DAP {{{1
+  "bash-debug-adapter",
+  "cpptools",
+  "debugpy",
+  "php-debug-adapter",
+
+  -- fmt {{{1
+  "autopep8",
+  "shfmt",
+
+  -- lint {{{1
+  "shellcheck",
+  "vale",
+  "vint",
+
+  -- {{{1
+  -- }}}1
+}
+
+
+vim.api.nvim_create_user_command("MasonEnsure", function()
+  vim.cmd("Mason")
+  local registry = require("mason-registry")
+  for _, package in ipairs(packages) do
+    if not registry.is_installed(package) then
+      registry.get_package(package):install()
+    end
+  end
+end, { nargs = 0 })
