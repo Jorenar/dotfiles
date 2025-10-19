e8b6f08e83fcd5faf46d27233bd1847c12939232
Jorenar <dev@jorenar.com>
2025-10-19 15:50:50 +0200

    Remove CommaFeed config

--- bin/cnt-commafeed-unread.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-[ -z "$COMMAFEED_URL$COMMAFEED_KEY" ] && exit 1
-
-curl -s "$COMMAFEED_URL/rest/category/unreadCount?apiKey=$COMMAFEED_KEY" | \
-    tr ',' '\n' | \
-    awk -F':' '/unreadCount/ {sum+=$2}END{print sum}'
